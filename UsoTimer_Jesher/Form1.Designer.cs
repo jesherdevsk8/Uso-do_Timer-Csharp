b5a17d2ec3faf3ed4001e1c880fe5ec1a7ceabf1
Jesher Minelli <jesherdevsk8@gmail.com>
2021-09-21 20:19:32 -0300

    Altera name e email

---
+++ UsoTimer_Jesher/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace UsoTimer_Jesher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer01 = new System.Windows.Forms.Timer(this.components);
+            this.btnon = new System.Windows.Forms.Button();
+            this.btnoff = new System.Windows.Forms.Button();
+            this.lblExibe = new System.Windows.Forms.Label();
+            this.lblTempo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer01
+            // 
+            this.timer01.Interval = 1000;
+            this.timer01.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // btnon
+            // 
+            this.btnon.Location = new System.Drawing.Point(71, 94);
+            this.btnon.Name = "btnon";
+            this.btnon.Size = new System.Drawing.Size(94, 31);
+            this.btnon.TabIndex = 0;
+            this.btnon.Text = "Turn On Timer";
+            this.btnon.UseVisualStyleBackColor = true;
+            this.btnon.Click += new System.EventHandler(this.Btnon_Click);
+            // 
+            // btnoff
+            // 
+            this.btnoff.Location = new System.Drawing.Point(71, 144);
+            this.btnoff.Name = "btnoff";
+            this.btnoff.Size = new System.Drawing.Size(94, 31);
+            this.btnoff.TabIndex = 1;
+            this.btnoff.Text = "Turn Off Timer";
+            this.btnoff.UseVisualStyleBackColor = true;
+            this.btnoff.Click += new System.EventHandler(this.Btnoff_Click);
+            // 
+            // lblExibe
+            // 
+            this.lblExibe.AutoSize = true;
+            this.lblExibe.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblExibe.Location = new System.Drawing.Point(56, 23);
+            this.lblExibe.Name = "lblExibe";
+            this.lblExibe.Size = new System.Drawing.Size(88, 15);
+            this.lblExibe.TabIndex = 2;
+            this.lblExibe.Text = "DateTimeNow";
+            // 
+            // lblTempo
+            // 
+            this.lblTempo.AutoSize = true;
+            this.lblTempo.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTempo.Location = new System.Drawing.Point(56, 50);
+            this.lblTempo.Name = "lblTempo";
+            this.lblTempo.Size = new System.Drawing.Size(104, 15);
+            this.lblTempo.TabIndex = 3;
+            this.lblTempo.Text = "Tempo Decorrido";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 205);
+            this.Controls.Add(this.lblTempo);
+            this.Controls.Add(this.lblExibe);
+            this.Controls.Add(this.btnoff);
+            this.Controls.Add(this.btnon);
+            this.Name = "Form1";
+            this.Text = "Timer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer01;
+        private System.Windows.Forms.Button btnon;
+        private System.Windows.Forms.Button btnoff;
+        private System.Windows.Forms.Label lblExibe;
+        private System.Windows.Forms.Label lblTempo;
+    }
+}
+
---
+++ UsoTimer_Jesher/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UsoTimer_Jesher
+{
+    public partial class Form1 : Form
+    {
+        Int32 isegundos = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            lblExibe.Text = DateTime.Now.ToString();
+            isegundos++;
+            TimeSpan t = TimeSpan.FromSeconds(isegundos);
+
+            string stempo = string.Format("{0:D2}h:{1:D2}m:{2:D2}s", t.Hours, t.Minutes, t.Seconds);
+
+            lblTempo.Text = stempo;            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // lblExibe.Text = DateTime.Now.ToString();
+            btnoff.Visible = false;
+        }
+
+        private void Btnon_Click(object sender, EventArgs e)
+        {
+            //timer01.Enabled = true;
+
+            //Método para ativar e desativar com um mesmo botão
+            timer01.Enabled = !timer01.Enabled;
+            if (timer01.Enabled == false)
+            { 
+                btnon.Text = "Ligar";
+                isegundos = 0;
+            }
+            else btnon.Text = "Desligar";
+        }
+
+        private void Btnoff_Click(object sender, EventArgs e)
+        {
+            timer01.Enabled = false;
+        }
+    }
+}
